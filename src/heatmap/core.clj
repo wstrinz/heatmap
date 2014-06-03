2608f7bb923b18b681edbebc182742b5f32aba7e
Will Strinz <wstrinz@gmail.com>
2014-06-03 15:22:38 -0500

    fetch a weeks worth of data

--- src/heatmap/core.clj
+++ src/heatmap/core.clj
@@ -15,23 +15,25 @@
 
 (def ex-day "20140501")
 
-(def formatter (f/formatter "yyyyMMdd"))
+(def input-formatter (f/formatter "yyyyMMdd"))
 
-(defn format-date [date] (f/unparse formatter date))
+(defn format-date [date] (f/unparse input-formatter date))
 
 (defn trackpoint_path [day] (str api-base daily-storyline-path "/" day token-query-param "&trackPoints=true"))
+
+(def week-str "2014-W22")
 
 (defn todays-storyline
   "fetch storyline from today"
   []
   (json/read-str
-   (->> (t/today-at 12 00)
-        (format-date)
+   (->> ;(t/today-at 12 00)
+        ;(format-date)
+        week-str
         (trackpoint_path)
         (client/get)
         (:body))
    :key-fn keyword))
-
 
 ((defn todays-trackpoints
    "extract trackpoints from storyline"
@@ -40,3 +42,8 @@
      (:segments)
      (map :activities) (flatten)
      (map :trackPoints) (flatten))))
+
+
+; assoc (?) f/parse :time
+; maybe aggregate nearby points?
+; display with the java/clojurescripts? http://www.patrick-wied.at/static/heatmapjs/index.html
