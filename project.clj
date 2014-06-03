b9604ca451a3b14c17ea48bd30abefdcd6fbbcd5
Will Strinz <wstrinz@gmail.com>
2014-06-03 11:21:01 -0500

    working trackPoint retrieval

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"][clj-http "0.9.2"][org.clojure/data.json "0.2.4"]])
+  :dependencies [ [org.clojure/clojure "1.5.1"]
+                  [clj-http "0.9.2"]
+                  [org.clojure/data.json "0.2.4"]
+                  [clj-time "0.7.0"] ])
--- src/heatmap/core.clj
+++ src/heatmap/core.clj
@@ -1,24 +1,40 @@
 (ns heatmap.core
-  (:require [clj-http.client :as client] [clojure.data.json :as json])
-  (:use [clojure.string]))
+  (:require [clj-http.client :as client]
+            [clojure.data.json :as json]
+            [clj-time.core :as t]
+            [clj-time.format :as f]
+            [clojure.string :as stri]))
 
 
 (def api-base "https://api.moves-app.com/api/1.1")
-
-(def daily-summary-path "/user/summary/daily")
-
-(def day "/20140501")
-
-(trim (slurp "oauth_key.txt"))
-(def token (trim (slurp "oauth_key.txt")))
-
+(def token (stri/trim (slurp "oauth_key.txt")))
 (def token-query-param (str "?access_token=" token))
 
-((defn todays-trackpoints
-  "fetch trackpoints from today"
-  []
-  (let [response (:body (client/get (str api-base daily-summary-path day token-query-param)))]
-    (json/read-str response :key-fn  keyword)
-    )))
+(def daily-summary-path "/user/summary/daily")
+(def daily-storyline-path "/user/storyline/daily")
 
-(todays-trackpoints)
+(def ex-day "20140501")
+
+(def formatter (f/formatter "yyyyMMdd"))
+
+(defn format-date [date] (f/unparse formatter date))
+
+(defn trackpoint_path [day] (str api-base daily-storyline-path "/" day token-query-param "&trackPoints=true"))
+
+(defn todays-storyline
+  "fetch storyline from today"
+  []
+  (let [ response
+         (:body (client/get (trackpoint_path (format-date (t/today-at 12 00)))))
+         ]
+    (json/read-str response :key-fn  keyword)
+    ))
+
+
+((defn todays-trackpoints
+   "extract trackpoints from storyline"
+   []
+   (->> (todays-storyline) (first)
+     (:segments)
+     (map :activities) (flatten)
+     (map :trackPoints) (flatten))))
